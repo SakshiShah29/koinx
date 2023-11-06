f1ae1a93d450f53df25bcc3069504c35f8575cd8
Sakshi Shah <sakshi.shah299@gmail.com>
2023-11-07 00:56:18 +0530

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Form from './components/Form';
 import logo from './logo.svg';
 import service from "./assets/1.svg"
+import email from "./assets/email.svg"
 import mobileview from "./assets/mobileview.svg"
 import Navbar from './components/Navbar';
 import Faq from './components/Faq';
@@ -12,6 +13,7 @@ function App() {
     base: mobileview,
     md: service, 
   });
+  const isDesktop = useBreakpointValue({ base: false, md: true });
   return (<div className='w-100%'>
     <Navbar/>
     <div className='bg-[#EFF2F5] h-fit  md:flex justify-center items-start pt-10 gap-10 md:px-[56px] p-[23px]'>
@@ -21,6 +23,8 @@ function App() {
     </div>
     <img src={serviceImage} alt="service"  width={'426px'}/>
     </div>
+   {isDesktop &&<div className='flex justify-center py-20'>
+   <img src={email} alt="email"  /></div>}
     <Footer/>
 
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,8 @@ export default function Navbar() {
         borderBottom={1}
         borderStyle={'solid'}
         borderColor={useColorModeValue('gray.200', 'gray.900')}
-        align={'center'}>
+        align={'center'}
+        justify={'space-between'}>
         
         <Flex flex={{ base: 1 }} justify={{ base: 'start', md: 'start' }}>
           <Image src={logo} alt="logo"/>
@@ -50,8 +51,7 @@ export default function Navbar() {
         
         <Flex
         flex={{ base: 1, md: 'auto' }}
-        ml={{ base: -2 }}
-        display={{ base: 'flex', md: 'none' }}>
+        display={{ base: 'flex', md: 'none' }} justify={{ base: 'end', md: 'end' }}>
         <IconButton
           onClick={onToggle}
           icon={isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />}
